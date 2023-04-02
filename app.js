ce68c50209a20495aadf443caede40222ac9b257
alanxurox <alanxu2013@gmail.com>
2023-04-02 23:14:09 +0200

    Init Commit

--- app.js
+++ app.js
@@ -1,25 +1,92 @@
 // we declare a new global variable containing an array that represents the ballons map
+const color = ["green", "red", "violet", "black", "yellow", "purple", "grey", "gray", "brown", "blue", "pink", "orange"];
+
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
+let ballonsAlive = 20;
+//generando de forma dinamica los colores
+for (let i=0; i<20; i++){
+    const randomColor = color[Math.floor(Math.random()*color.length)]
+    let ballon = document.createElement("div");
+    ballon.classList.add('balloon', 'balloon');
+    ballon.style.background = randomColor;
+    ballon.id = i;
 
-// poping a balloon is basically turning his color to null (no color)
-const popBalloon = (position) => {
-    // set the color to null on the balloon position
-    render();
-}
+    // Agregar EventListener a cada ballon:
+    ballon.addEventListener('click', (e) => {
+        ballon.style.visibility = 'hidden';
+        ballonsAlive--;
 
-const render = () => {
-    
-    // convert ballons map of colors into real html balloons
-    const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        if(ballonsAlive == 0) render();
+        const pop = new AudioContext();
+        fetch('./pop.mp3')
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            pop.decodeAudioData(buffer, function(decodedData) {
+                const source = pop.createBufferSource();
+                source.buffer = decodedData;
+                source.connect(pop.destination);
+                source.start(0);
+            });
+        })
+        .catch(err => console.log(err));
     });
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
-    document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+    // Agregar cada ballon a la lista de ballons:
+    document.querySelector("#balloon-map").appendChild(ballon);
+  }
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+const render = () => {
+    document.querySelector("#balloon-count").innerHTML = ballonsAlive; // <-- render the balloon count into the DOM
+
+    if(ballonsAlive == 0) {
+        const music = new AudioContext();
+        fetch('./music.mp3')
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            music.decodeAudioData(buffer, function(decodedData) {
+                const source = music.createBufferSource();
+                source.buffer = decodedData;
+                source.connect(music.destination);
+                source.start(0);
+            });
+        })
+        .catch(err => console.log(err));
+        window.location.reload(); // <--- reload website when no more balloons are left
+    }
 }
 
 // this makes the "render" function trigger when the website starts existing
-window.onload = render();
+window.onload = () => {
+    const music = new AudioContext();
+    fetch('./music.mp3')
+    .then(response => response.arrayBuffer())
+    .then(buffer => {
+        music.decodeAudioData(buffer, function(decodedData) {
+            const source = music.createBufferSource();
+            source.buffer = decodedData;
+            source.connect(music.destination);
+            source.start(0);
+        });
+    })
+    .catch(err => console.log(err));
+    render();
+};
+
+// add a button to enable audio, and when user clicks it, unmute all audio; set all audio to mute at start
+const audioElements = document.querySelectorAll("audio");
+
+audioElements.forEach(audio => {
+    audio.muted = true;
+});
+
+const audioButton = document.querySelector("#audioButton");
+
+if (audioButton) {
+    if (audioButton.addEventListener) {
+        audioButton.addEventListener("click", () => {
+            audioElements.forEach(audio => {
+                audio.muted = false;
+            });
+        });
+    }
+}
