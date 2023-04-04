82e8241bae27ad677943eea851b910eed6163205
alanxurox <alanxu2013@gmail.com>
2023-04-04 05:41:58 +0200

    Add 5s timer

--- app.js
+++ app.js
@@ -90,3 +90,50 @@ if (audioButton) {
         });
     }
 }
+
+// add a mobile UI friendly timer that shows a countdown from 5 to 0s, showing the ms, and starts when user clicks the first balloon
+let timer = document.createElement("div");
+timer.classList.add('timer');
+document.querySelector("#balloon-map").appendChild(timer);
+
+let startTimer = false;
+let timeLeft = 5000;
+
+const startCountdown = () => {
+    startTimer = true;
+    const startTime = Date.now();
+    const interval = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        timeLeft = Math.max(0, 5000 - elapsedTime);
+        const seconds = Math.floor(timeLeft / 1000);
+        const milliseconds = timeLeft % 1000;
+        timer.innerHTML = `${seconds}:${milliseconds.toString().padStart(3, '0')}`;
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            timer.innerHTML = "Time's up!";
+        }
+    }, 10);
+}
+
+document.querySelectorAll('.balloon').forEach(balloon => {
+    balloon.addEventListener('click', () => {
+        if (!startTimer) {
+            startCountdown();
+        }
+        balloon.style.visibility = 'hidden';
+        ballonsAlive--;
+        if(ballonsAlive == 0) render();
+        const pop = new AudioContext();
+        fetch('./pop.mp3')
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            pop.decodeAudioData(buffer, function(decodedData) {
+                const source = pop.createBufferSource();
+                source.buffer = decodedData;
+                source.connect(pop.destination);
+                source.start(0);
+            });
+        })
+        .catch(err => console.log(err));
+    });
+}); 
